e695f9f4742f313b9a2e6599f89369200f88827f
Danilo <daniloaugusto0212@hotmail.com>
2021-01-21 22:20:33 -0300

    Finalizando front

--- index.php
+++ index.php
@@ -56,6 +56,53 @@
                     </div>
 
                     <div class="w100">
+                        <h2>Data de Nascimento:</h2>
+
+                        <select name="nascimento-dia" class="nascimento">
+                            <?php
+                            for ($i = 1; $i <= 31; $i++) { 
+                                ?>
+                                <option value="<?= $i ?>"><?= $i ?></option>
+                            <?php } ?>
+                        </select>
+
+                        <select name="nascimento-mes" class="nascimento">
+                            <option value="1">Janeiro</option>
+                            <option value="2">Fevereiro</option>
+                            <option value="3">Março</option>
+                            <option value="4">Abril</option>
+                            <option value="5">Maio</option>
+                            <option value="6">Junho</option>
+                            <option value="7">Julho</option>
+                            <option value="8">Agosto</option>
+                            <option value="9">Setembro</option>
+                            <option value="10">Outubro</option>
+                            <option value="11">Novembro</option>
+                            <option value="12">Dezembro</option>
+                        </select>
+                        <select name="nascimento-ano" class="nascimento">
+                            <?php
+                            for ($i = 1960; $i <= date('Y'); $i++) { 
+                                ?>
+                                <option value="<?= $i ?>"><?= $i ?></option>
+                            <?php } ?>
+                        </select>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="w100">
+                        <div class="input-radio">
+                            <input type="radio" name="sexo" value="masculino">
+                            <h2>Masculino</h2>
+                        </div>
+                        <div class="input-radio">
+                            <input type="radio" name="sexo" value="feminino">
+                            <h2>Feminino</h2>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="w100">
                         <input type="submit" value="Cadastrar!">
                     </div>
 
@@ -67,5 +114,26 @@
         </div>
         <div class="clear"></div>
     </section>
+
+    <section class="linguas">
+        <div class="center">
+            <a class="selected-lingua" href="#">Português (BR)</a>
+            <a href="#">Inglês</a>
+            <a href="#">Chinês Mandarim</a>
+            <a href="#">Hindi </a>
+            <a href="#">Espanhol </a>
+            <a href="#">Francês </a>
+            <a href="#">Árabe </a>
+        </div>
+
+        <div style="border: 0; padding-top:10px;" class="center">
+        <?php
+        for ($i = 0; $i < 35; $i++) {
+            ?>
+            <a href="#">Ipsum excepteur</a>
+        <?php } ?>
+        </div>
+    </section>
+    
 </body>
 </html>
